ebb55c64c437d8fa5cab432ac43c7cc9a5d595d9
manish pradhan <pradhanmanish33@gmail.com>
2016-10-20 00:19:42 -0400

    commit

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
   has_many :comments, dependent: :destroy
   validates :name, presence: true, length: {minimum: 5}
   validates :ingredients, presence: true
