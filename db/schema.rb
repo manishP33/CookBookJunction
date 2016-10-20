712cbc397c26a1c1cddbecbcfc79aa6abc2a9d1a
manish pradhan <pradhanmanish33@gmail.com>
2016-10-20 08:38:42 -0400

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20161019185306) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string   "name"
     t.text     "body"
     t.integer  "recipe_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["recipe_id"], name: "index_comments_on_recipe_id"
+    t.index ["recipe_id"], name: "index_comments_on_recipe_id", using: :btree
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -43,8 +46,9 @@ ActiveRecord::Schema.define(version: 20161019185306) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "comments", "recipes"
 end
